be29584cd62bf3bf6d4851834114bef0ad0008da
Iuri Ostrikov <iurii84@gmail.com>
2019-07-01 22:28:43 -0400

    some Meal JPA fixes

--- src/main/java/model/Meal.java
+++ src/main/java/model/Meal.java
@@ -1,5 +1,7 @@
 package model;
 
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 import org.hibernate.validator.constraints.Range;
 
 import javax.persistence.*;
@@ -8,24 +10,25 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Entity
-@Table(name = "meal")
+@Table(name = "meals")
 public class Meal {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;                                        //ID
 
-    @Column(name = "description")
+    @Column(name = "description", nullable = false)
     @NotBlank
     @Size(min = 5, max = 100)
     private String description;                             //Description of the meal
 
-    @Column(name = "price")
+    @Column(name = "price", nullable = false)
     @NotNull
     @Range(max = 200_00)
     private Long price;                                     //price in cents (max price 200$)
 
     @ManyToOne(fetch = FetchType.LAZY)                      //reference for the restaurants id, that proposes that meal
-    @JoinColumn(name = "resto_id", referencedColumnName = "id")
+    @JoinColumn(name = "resto_id", referencedColumnName = "id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
     private Restaurant restaurant;
 
 }
