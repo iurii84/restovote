a89c6957d57eba308c85fe70704785a8f6d071e3
Iuri Ostrikov <iurii84@gmail.com>
2019-07-01 22:39:09 -0400

    Winner JPA some fixes

--- src/main/java/model/Winner.java
+++ src/main/java/model/Winner.java
@@ -15,10 +15,10 @@ public class Winner {
     private Long id;
 
 
-    @Column(name = "date")
+    @Column(name = "date", nullable = false)
     private LocalDate localDate;                            //Date when the restaurant become the winner
 
-    @Column (name = "restaurant")
+    @Column (name = "restaurant", nullable = false)
     private Restaurant restaurant;                          //get entire entity of restaurant. Even restaurant is removed
                                                             //we can see all statistics of voting
 
