c6f3a70a65a236ca28f3c9648065a5f2e7021cb7
Iuri Ostrikov <iurii84@gmail.com>
2019-07-01 23:15:34 -0400

    User JPA implementation

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,16 +1,45 @@
 package model;
 
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 import java.util.Set;
 
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)                     //ID
     private Long id;
-    private String name;
-    private String email; //as identificator
-    private String password;
 
-    private Set<Role> roles;
+    @Column(name = "name", nullable = false)
+    @NotBlank
+    @Size (min = 7, max = 20)
+    private String name;                                                //the full name of person
 
-    private LocalDateTime dateTimeOfVote;
-    private Restaurant votedFor;
+    @Column(name = "email", nullable = false, unique = true)
+    @NotBlank
+    @Email
+    @Size(min = 8, max = 20)
+    private String email;                                               //unique email - used for identification in the system
+
+    @Column(name = "password", nullable = false)
+    @NotBlank
+    @Size(min = 5, max = 100)
+    private String password;                                            //password of user
+
+    @Enumerated(EnumType.STRING)
+    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user.id"))
+    @Column(name = "role")
+    @ElementCollection(fetch = FetchType.EAGER)
+    private Set<Role> roles;                                            //user role from ENUM "Role"
+
+    @Column(name = "dateTimeOfVote", nullable = false)
+    private LocalDateTime dateTimeOfVote;                               //date and time when that user has voted for
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinColumn(name = "resto_id", referencedColumnName = "id", nullable = false)
+    private Restaurant votedFor;                                        //restaurant id, which were voted by user
 }
