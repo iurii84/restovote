2165eadd61f452b59dee7c5b8efef25bc8164821
Iuri Ostrikov <iurii84@gmail.com>
2019-07-02 18:24:47 -0400

    Restaurant JPA implementation

--- src/main/java/model/Restaurant.java
+++ src/main/java/model/Restaurant.java
@@ -1,10 +1,28 @@
 package model;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.List;
-
+@Entity
+@Table(name = "restaurants")
 public class Restaurant {
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    private Long id;                                    //ID
+
+    @Column(name = "name", nullable = false)
+    @NotBlank
+    @Size(min = 5, max = 30)
+    private String name;                                //name of restaurant
+
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "meal_id", nullable = false)
     private List<Meal> mealList;
 
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id", nullable = false)
     private List<User> listOfUsersVotedFor;
 }
