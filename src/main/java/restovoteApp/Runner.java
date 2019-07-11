fb45dd89c2a7babaa9b89bf8aef87cdca84be0ff
Iuri Ostrikov <iurii84@gmail.com>
2019-07-11 18:06:15 -0400

    TODO - initialize project

---
+++ src/main/java/restovoteApp/Runner.java
@@ -0,0 +1,31 @@
+package restovoteApp;
+
+import restovoteApp.controller.MealRestController;
+import restovoteApp.model.Meal;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import restovoteApp.repository.MealRepositoryInterfaceImpl;
+
+public class Runner {
+    private static ClassPathXmlApplicationContext springContext;
+    private static MealRestController mealController;
+
+
+    private static Meal meal_1 = new Meal(null, "description_1", 2000L);
+    private static Meal meal_2 = new Meal(null, "description_2", 3000L);
+    private static Meal meal_3 = new Meal(null, "description_3", 4000L);
+
+    public static void main(String[] args) {
+//        springContext = new ClassPathXmlApplicationContext("spring-db.xml");
+//        mealController = springContext.getBean(MealRestController.class);
+
+
+
+
+
+        MealRepositoryInterfaceImpl repo = new MealRepositoryInterfaceImpl();
+        repo.save(meal_1, 1L, 1L);
+        repo.save(meal_2, 2L, 2L);
+        repo.save(meal_3, 3L, 3L);
+    }
+
+}
