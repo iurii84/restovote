d8b93026867380e4631161b93ee9706904d80561
Iuri Ostrikov <iurii84@gmail.com>
2019-09-24 20:46:01 -0400

    Unit Test

---
+++ src/test/java/restovoteApp/controller/AdminUserControllerTest.java
@@ -0,0 +1,17 @@
+package restovoteApp.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+class AdminUserControllerTest extends AbstractControllerTest {
+
+    private static final String URL = "/admin/users/1";
+
+    @Test
+    void editUser() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post(URL))
+                .andDo(print());
+    }
+}
