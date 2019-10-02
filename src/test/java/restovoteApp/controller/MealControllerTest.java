c9d728da41b779660b7a784748d27c82fd6ea819
Iuri Ostrikov <iurii84@gmail.com>
2019-10-01 21:13:22 -0400

    Unit Test

--- src/test/java/restovoteApp/controller/MealControllerTest.java
+++ src/test/java/restovoteApp/controller/MealControllerTest.java
@@ -1,8 +1,10 @@
 package restovoteApp.controller;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.security.test.context.support.WithMockUser;
 import restovoteApp.model.User;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
@@ -13,9 +15,13 @@ private final User USER = new User();
     void getMealById() {
     }
 
+    @WithMockUser(value = "ROLE_USER")
     @Test
     void getAllMealByRestoID() throws Exception{
-        mockMvc.perform(get("/meal/resto/1"))
+        mockMvc.perform(get("/meal/resto/1")
+                .with(httpBasic("tanea@gmail.com", "321321321")))
                 .andDo(print());
+
+
     }
 }
