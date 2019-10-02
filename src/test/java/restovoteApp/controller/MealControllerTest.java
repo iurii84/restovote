b10276f56ef7b0d37d24f0aa460b0df1d62ea264
Iuri Ostrikov <iurii84@gmail.com>
2019-10-01 21:21:32 -0400

    Unit Test

--- src/test/java/restovoteApp/controller/MealControllerTest.java
+++ src/test/java/restovoteApp/controller/MealControllerTest.java
@@ -1,26 +1,32 @@
 package restovoteApp.controller;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import restovoteApp.model.User;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 class MealControllerTest extends AbstractControllerTest {
 
-private final User USER = new User();
+    private final User USER = new User();
+
     @Test
     void getMealById() {
     }
 
     @WithMockUser(value = "ROLE_USER")
     @Test
-    void getAllMealByRestoID() throws Exception{
+    void getAllMealByRestoID() throws Exception {
         mockMvc.perform(get("/meal/resto/1")
                 .with(httpBasic("tanea@gmail.com", "321321321")))
-                .andDo(print());
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
 
 
     }
