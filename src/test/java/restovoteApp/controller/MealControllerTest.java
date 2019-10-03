9cb52ba63d1c50a83defc2c9794df3647b339a1f
Iuri Ostrikov <iurii84@gmail.com>
2019-10-02 21:42:51 -0400

    test

--- src/test/java/restovoteApp/controller/MealControllerTest.java
+++ src/test/java/restovoteApp/controller/MealControllerTest.java
@@ -1,33 +1,47 @@
 package restovoteApp.controller;
 
+import net.minidev.json.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithMockUser;
 import restovoteApp.model.User;
 
+import static org.hamcrest.Matchers.hasItem;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 class MealControllerTest extends AbstractControllerTest {
 
     private final User USER = new User();
+    private static JSONObject jsonObject = new JSONObject();
+
+    static {
+
+        jsonObject.put("id", 1);
+        jsonObject.put("description", "eggs");
+        jsonObject.put("price", 130);
+        jsonObject.put("new", false);
+    }
 
     @Test
     void getMealById() {
     }
 
-    @WithMockUser(value = "ROLE_USER")
+
     @Test
     void getAllMealByRestoID() throws Exception {
-        mockMvc.perform(get("/meal/resto/1")
-                .with(httpBasic("tanea@gmail.com", "321321321")))
+        mockMvc.perform(get("/meal/resto/1").with(httpBasic("tanea@gmail.com", "321321321")))
                 .andExpect(status().isOk())
                 .andDo(print())
-                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$", hasItem(jsonObject)));
+    }
 
-
+    @Test
+    void unauthorisedAccess() throws Exception {
+        mockMvc.perform(get("/meal/resto/1").with(httpBasic("wrong@email.com", "wrongpassword")))
+                .andExpect(status().isUnauthorized())
+                .andDo(print());
     }
 }
